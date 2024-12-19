dcbf318e83a45c7b0823c3bbf01e21adc3678191
Gursharan Singh <gs7217013@gmail.com>
2024-12-20 00:08:03 +0100

    20th december- added separation of concerns

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-
+const db = require('./config/mongoose-connection');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const ownersRouter = require('./routes/ownersRouter');
+const usersRouter = require('./routes/usersRouter');
+const productsRouter = require('./routes/productsRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -11,8 +13,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.send('Hey Guri!');
-});
+app.use('/owners', ownersRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 app.listen(3000);
