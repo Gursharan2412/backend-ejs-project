da5e2989b029aa1344cbe54b5129e985807b313b
Gursharan Singh <gs7217013@gmail.com>
2024-12-21 12:17:15 +0100

    Added .gitignore and removed node_modules from tracking

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
+const dbgr = require("debug")("development:mongoose");
+const debug = require('debug');
+debug.enable('development:*');
+
 
 mongoose
 .connect('mongodb://127.0.0.1:27017/scatch')
 .then(function(){
-    console.log("connected");
+    debug("connected");
 })
 .catch(function(err){
-    console.log(err);
+    debug(err);
 })
 
 module.exports = mongoose.connection;
