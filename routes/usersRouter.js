349e33bbd193edc48d3f7562d59a2a34381318bb
Gursharan Singh <gs7217013@gmail.com>
2025-01-04 23:34:53 +0100

    Added untracked and modified files with production level code format

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser } = require('../controllers/authController');
+const { loginUser } = require('../controllers/authController');
 
 router.get('/', function(req, res){
     res.send('Hey its working...');
 });
+
+router.post('/register', registerUser);
+
+router.post('/login', loginUser);
+
 
 module.exports = router;
